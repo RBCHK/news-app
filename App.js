b8cf78950ecaf5aa1eb8c6088d281b18b3a68c49
Artem Rybachuk <artem.rybachuk@gmail.com>
2023-07-22 20:38:02 +0300

    created post component

--- App.js
+++ App.js
@@ -1,20 +1,43 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+import { Alert, Text } from 'react-native';
+import { Wrapper } from './Styled';
+import Post from './components/Post';
+
+const data = {
+	title: 'Well Hello, Sophia Smith',
+	subTitle:
+		'My old hoops coach, the late Hall of Famer Pete Carril, used to call them “light bulbs.” They were the players who’d walk onto the court in practice and make him light up, even if he were.. ',
+	createdAt: '05/04/2023',
+	readingTime: '1 min',
+	imageUrl: 'https://api.time.com/wp-content/uploads/2023/07/GettyImages-1562395034.jpg?',
+};
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+	const [items, setItems] = useState();
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+	useEffect(() => {
+		axios
+			.get('https://64bc008b7b33a35a4446e91b.mockapi.io/news')
+			.then(({ data }) => {
+				setItems(data);
+			})
+			.catch(() => {
+				Alert.alert('Alert', 'Error while getting news');
+			});
+	}, []);
+
+	const { title, subTitle, createdAt, readingTime, imageUrl } = data;
+	return (
+		<Wrapper>
+			<Text style={{ color: 'white', marginTop: 55, marginBottom: 5 }}>Back</Text>
+			<Post
+				title={title}
+				imageUrl={imageUrl}
+				subTitle={subTitle}
+				createdAt={createdAt}
+				readingTime={readingTime}
+			/>
+		</Wrapper>
+	);
+}
---
+++ Styled.js
@@ -0,0 +1,6 @@
+import styled from 'styled-components';
+export const Wrapper = styled.View`
+	flex: 1;
+	background-color: #121212;
+	padding: 0 10px;
+`;
